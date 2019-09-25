2c2c4b7dbe3bb616e82874bee829ad94c75b1a9a
Bexultan Myrzatayev <a.e.iimitk@gmail.com>
2019-09-25 16:26:46 +0600

    feat: render ActivityIndicator on loading and setup validation

--- components/application-create/application-create-styles.js
+++ components/application-create/application-create-styles.js
@@ -4,15 +4,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginHorizontal: 16,
-    marginVertical: 20,
+    paddingTop: 20,
     backgroundColor: '#fff',
+    justifyContent: 'center'
+  },
+  header: {
+    textAlign:'center',
+    fontSize: 24,
+    fontWeight: '700',
+    marginBottom: 24
   },
   textInput: {
     height: 40, borderColor: 'gray', borderWidth: 1,
-    marginBottom: 16
+    marginBottom: 16,
+    paddingHorizontal: 16
   },
   button: {
-    backgroundColor: 'red'
+    
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50
+  },
+  buttonDisabled: {
+backgroundColor: "#8D0D26"
+  },
+  buttonEnabled: {
+    backgroundColor: '#DC143C',
+  },
+  buttonText: {
+    fontWeight: '700',
+    fontSize: 16,
+    color: 'white'
   }
 });
 
--- components/application-create/application-create-view.js
+++ components/application-create/application-create-view.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { TextInput, Button,  SafeAreaView } from 'react-native';
+import { TextInput, ActivityIndicator,  SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
 import { useMutation } from '@apollo/react-hooks';
 
 import styles from './application-create-styles'
@@ -11,36 +11,68 @@ const ApplicationCreate = () => {
   const [address, setAddress] = useState("")
 
   const [createApplication, { loading }] = useMutation(
-    mutations.APPLICATION_CREATE_MUTATION
+    mutations.APPLICATION_CREATE_MUTATION,
+    {
+      variables: { title, description, address },
+      onCompleted: ({ createApplication: completedCreateApplication }) => {
+        if (completedCreateApplication) {
+          setTitle("")
+          setDescription("")
+          setAddress("")
+          alert("Заявление отправлено")
+        } else {
+          console.log("error", "Could not create application");
+        }
+      }
+    }
   );
 
-  const submitCreateApplicationForm = () => {
-    createApplication({ variables: { title, description, address } });
-  }
+  const disabled = () => loading || title === "" || description === "" || address === ""
+
+  if (loading)
+    return (
+      <View style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1
+      }}>
+<ActivityIndicator size="large" color="#DC143C" />
+      </View>
+
+    ) 
 
   return (
-
       <SafeAreaView style={styles.container}>
+      <View style={styles.formContainer}>
+      <Text style={styles.header}>Заявление</Text>
         <TextInput
         style={styles.textInput}
           onChangeText={text => setTitle(text)}
           value={title}
+          placeholder="Заголовок..."
         />
         <TextInput
         style={styles.textInput}
           onChangeText={text => setDescription(text)}
           value={description}
+          placeholder="Описание..."
         />
         <TextInput
         style={styles.textInput}
           onChangeText={text => setAddress(text)}
           value={address}
+          placeholder="Адрес..."
         />
-        <Button
-          title="Отправить"
-          style={styles.button}
-          onPress={submitCreateApplicationForm}
-        />
+        <TouchableOpacity
+          style={[styles.button, disabled() ? styles.buttonDisabled : styles.buttonEnabled]}
+          onPress={createApplication}
+          disabled={
+            disabled()
+          }
+        >
+        <Text  style={styles.buttonText}>Отправить</Text>
+        </TouchableOpacity>
+        </View>
       </SafeAreaView>
   );
 }
